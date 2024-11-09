0b5f9b3e34fb17377a451d484db2ed5760c9f904
Gabriel_Oliveira_Bromo <gabrielbromo@outlook.com>
2024-11-09 10:08:47 -0300

    add faker generate, forms,validation

--- constants.js
+++ constants.js
@@ -1,2 +1,4 @@
 //FbfLnU22homn0Hv4
-export const transactionView = ['Yearly','Monthly','Daily'];
+export const transactionViewOptions = ['Yearly','Monthly','Daily'];
+export const categories = ['Food','Housing','Car','Entertainment'];
+export const types = ['Icome','Expense','Saving','Investment'];
